73b5d51073c74e304f2e7bd2f58ac0b754fa28e2
Shriram Kalimuthan <Shriram Kalimuthan>
2017-04-09 17:48:29 -0600

    removed http redirection

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-var express =require('express');
+var express = require('express');
 
-var app =express();
-const PORT =process.env.PORT || 3000;
-
-app.use(function(req,res,next) {
-
-    if(req.header['x-forwarded-proto'] === 'http'){
-        next();
-
-    }else{
-        res.redirect('http://'+req.hostname+req.url);
-
-    }
-
-});
+// Create our app
+var app = express();
+const PORT = process.env.PORT || 3000;
 
 
 
 app.use(express.static('public'));
 
-app.listen(PORT, function(){
-    console.log('Express Server is running on the port '+ PORT);
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
